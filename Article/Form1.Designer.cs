161e027401ba87bb932ae206278b8d291c1de0ca
jash-git <jash.liao@gmail.com>
2017-07-07 08:45:28 +0800

    Create
    https://github.com/jash-git/CS_ListView_Show_Type.git

---
+++ Article/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Article
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.myListView = new System.Windows.Forms.ListView();
+            this.rbLargeIcon = new System.Windows.Forms.RadioButton();
+            this.rbList = new System.Windows.Forms.RadioButton();
+            this.rbSmallIcon = new System.Windows.Forms.RadioButton();
+            this.rbTile = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // myListView
+            // 
+            this.myListView.Location = new System.Drawing.Point(12, 12);
+            this.myListView.Name = "myListView";
+            this.myListView.Size = new System.Drawing.Size(346, 485);
+            this.myListView.TabIndex = 0;
+            this.myListView.UseCompatibleStateImageBehavior = false;
+            this.myListView.SelectedIndexChanged += new System.EventHandler(this.myListView_SelectedIndexChanged);
+            // 
+            // rbLargeIcon
+            // 
+            this.rbLargeIcon.AutoSize = true;
+            this.rbLargeIcon.Checked = true;
+            this.rbLargeIcon.Location = new System.Drawing.Point(382, 13);
+            this.rbLargeIcon.Name = "rbLargeIcon";
+            this.rbLargeIcon.Size = new System.Drawing.Size(76, 17);
+            this.rbLargeIcon.TabIndex = 1;
+            this.rbLargeIcon.TabStop = true;
+            this.rbLargeIcon.Text = "Large Icon";
+            this.rbLargeIcon.UseVisualStyleBackColor = true;
+            this.rbLargeIcon.CheckedChanged += new System.EventHandler(this.rbLargeIcon_CheckedChanged);
+            // 
+            // rbList
+            // 
+            this.rbList.AutoSize = true;
+            this.rbList.Location = new System.Drawing.Point(382, 36);
+            this.rbList.Name = "rbList";
+            this.rbList.Size = new System.Drawing.Size(41, 17);
+            this.rbList.TabIndex = 2;
+            this.rbList.Text = "List";
+            this.rbList.UseVisualStyleBackColor = true;
+            this.rbList.CheckedChanged += new System.EventHandler(this.rbList_CheckedChanged);
+            // 
+            // rbSmallIcon
+            // 
+            this.rbSmallIcon.AutoSize = true;
+            this.rbSmallIcon.Location = new System.Drawing.Point(382, 59);
+            this.rbSmallIcon.Name = "rbSmallIcon";
+            this.rbSmallIcon.Size = new System.Drawing.Size(74, 17);
+            this.rbSmallIcon.TabIndex = 3;
+            this.rbSmallIcon.Text = "Small Icon";
+            this.rbSmallIcon.UseVisualStyleBackColor = true;
+            this.rbSmallIcon.CheckedChanged += new System.EventHandler(this.rbSmallIcon_CheckedChanged);
+            // 
+            // rbTile
+            // 
+            this.rbTile.AutoSize = true;
+            this.rbTile.Location = new System.Drawing.Point(382, 82);
+            this.rbTile.Name = "rbTile";
+            this.rbTile.Size = new System.Drawing.Size(42, 17);
+            this.rbTile.TabIndex = 4;
+            this.rbTile.Text = "Tile";
+            this.rbTile.UseVisualStyleBackColor = true;
+            this.rbTile.CheckedChanged += new System.EventHandler(this.rbTile_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(477, 509);
+            this.Controls.Add(this.rbTile);
+            this.Controls.Add(this.rbSmallIcon);
+            this.Controls.Add(this.rbList);
+            this.Controls.Add(this.rbLargeIcon);
+            this.Controls.Add(this.myListView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView myListView;
+        private System.Windows.Forms.RadioButton rbLargeIcon;
+        private System.Windows.Forms.RadioButton rbList;
+        private System.Windows.Forms.RadioButton rbSmallIcon;
+        private System.Windows.Forms.RadioButton rbTile;
+    }
+}
+
---
+++ Article/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.ComponentModel;
+//using System.Data;
+using System.Drawing;
+//using System.Linq;
+//using System.Text;
+using System.Windows.Forms;
+
+namespace Article
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            //Take imagelist for store images to use in listview.
+            System.Windows.Forms.ImageList myImageList1 = new ImageList();
+            //set the image size of which size images will be displayed in the listview.
+            myImageList1.ImageSize = new Size(64, 64);
+
+            //Now add the images into the imageList..
+            myImageList1.Images.Add(Image.FromFile(@"01.PNG"));
+            myImageList1.Images.Add(Image.FromFile(@"02.PNG"));
+            myImageList1.Images.Add(Image.FromFile(@"03.PNG"));
+            myImageList1.Images.Add(Image.FromFile(@"04.PNG"));
+            myImageList1.Images.Add(Image.FromFile(@"05.PNG"));
+            myImageList1.Images.Add(Image.FromFile(@"06.PNG"));
+            myImageList1.Images.Add(Image.FromFile(@"07.PNG"));
+            myImageList1.Images.Add(Image.FromFile(@"08.PNG"));
+            myImageList1.Images.Add(Image.FromFile(@"09.PNG"));
+            myImageList1.Images.Add(Image.FromFile(@"10.PNG"));
+            
+            
+
+            //Take another imageList to also display in the listview..
+            System.Windows.Forms.ImageList myImageList2 = new ImageList();
+            //set the image size smaller than the first imageList...
+            myImageList2.ImageSize = new Size(32, 32);
+            
+            //Add images into the second imageList...
+            myImageList2.Images.Add(Image.FromFile(@"01.PNG"));
+            myImageList2.Images.Add(Image.FromFile(@"02.PNG"));
+            myImageList2.Images.Add(Image.FromFile(@"03.PNG"));
+            myImageList2.Images.Add(Image.FromFile(@"04.PNG"));
+            myImageList2.Images.Add(Image.FromFile(@"05.PNG"));
+            myImageList2.Images.Add(Image.FromFile(@"06.PNG"));
+            myImageList2.Images.Add(Image.FromFile(@"07.PNG"));
+            myImageList2.Images.Add(Image.FromFile(@"08.PNG"));
+            myImageList2.Images.Add(Image.FromFile(@"09.PNG"));
+            myImageList2.Images.Add(Image.FromFile(@"10.PNG"));
+
+
+            //Now assing First imageList as LargeImageList to the ListView...
+            myListView.LargeImageList = myImageList1;
+
+            //assing the second imageList as SmallImageList to the ListView...
+            myListView.SmallImageList  = myImageList2;
+            
+
+            //Then Add the items to the Listview & also assign the image index to that item...
+            myListView.Items.Add("Item 1", 0);
+            myListView.Items.Add("Item 2", 1);
+            myListView.Items.Add("Item 3", 2);
+            myListView.Items.Add("Item 4", 3);
+            myListView.Items.Add("Item 5", 4);
+            myListView.Items.Add("Item 6", 5);
+            myListView.Items.Add("Item 7", 6);
+            myListView.Items.Add("Item 8", 7);
+            myListView.Items.Add("Item 9", 8);
+            myListView.Items.Add("Item 10", 9);
+
+
+            //You can also create the Groups into the ListView. if you want to add the Groups into the Listview then below
+            //code will helpful for you.
+            //In the below code i have created two ListViewGroup with it's name & it's alignment in ListView...
+            ListViewGroup myLVGroup1 = new ListViewGroup("First Five Group", HorizontalAlignment.Left);
+            ListViewGroup myLVGroup2 = new ListViewGroup("Last Five Group", HorizontalAlignment.Left);
+            //after creating the ListViewGroup as you want add that group to the ListView...
+            myListView.Groups.AddRange(new ListViewGroup[] { myLVGroup1, myLVGroup2 });
+
+
+            //Finally assign the each item in wich group...
+
+            //In below code first five items will be in the First group..
+            myListView.Items[0].Group = myListView.Groups[0];
+            myListView.Items[1].Group = myListView.Groups[0];
+            myListView.Items[2].Group = myListView.Groups[0];
+            myListView.Items[3].Group = myListView.Groups[0];
+            myListView.Items[4].Group = myListView.Groups[0];
+
+            //Last Five items will be in the Second Group...
+            myListView.Items[5].Group = myListView.Groups[1];
+            myListView.Items[6].Group = myListView.Groups[1];
+            myListView.Items[7].Group = myListView.Groups[1];
+            myListView.Items[8].Group = myListView.Groups[1];
+            myListView.Items[9].Group = myListView.Groups[1];
+           
+        }
+
+        //When user will select the "Large Icon" RadioButton, then it will display items into Large Icon view...
+        private void rbLargeIcon_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbLargeIcon.Checked == true)
+            {
+                myListView.View = View.LargeIcon;
+            }
+        }
+
+        //When user will select the "List" RadioButton, then it will display items into List view...
+        private void rbList_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbList.Checked == true)
+            {
+                myListView.View = View.List;
+            }
+        }
+
+        //When user will select the "Small Icon" RadioButton, then it will display items into Small Icon view...
+        private void rbSmallIcon_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbSmallIcon.Checked == true)
+            {
+                myListView.View = View.SmallIcon;
+            }
+        }
+
+        //When user will select the "Tile" RadioButton, then it will display items into Tile view...
+        private void rbTile_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbTile.Checked == true)
+            {
+                myListView.View = View.Tile;
+            }
+        }
+
+
+        //Finally it will display the selected item message box when user will select a particular item from the ListView...
+        private void myListView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            foreach (ListViewItem item in myListView.SelectedItems)
+            {
+                MessageBox.Show(item.Text.ToString());
+            }
+        }
+    }
+}
